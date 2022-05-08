aeb44b3a48f27e5ee475c399112551e5fdf68827
unknowncoder05 <yersonlasso05@gmail.com>
2022-05-08 08:27:17 -0500

    git default settings

--- dev/git.sh
+++ dev/git.sh
@@ -1,2 +1,5 @@
 sudo apt-get install git
 add-apt-repository ppa:git-core/ppa # apt update; apt install git
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
