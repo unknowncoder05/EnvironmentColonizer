9cd909fd69f28c2e79ed0d669adc5a020deb3a4e
unknowncoder05 <yersonlasso05@gmail.com>
2022-05-08 09:50:57 -0500

    more development tools

---
+++ dev/github.sh
@@ -0,0 +1,9 @@
+brew install gh
+
+gh auth login
+
+sudo apt install libedit-dev
+sudo apt install libncurses5-dev
+
+pyenv install 3.10.4
+pyenv global 3.10.4
---
+++ dev/ssh.sh
@@ -0,0 +1,3 @@
+ssh-keygen -t ed25519 -C "yersonlasso05@gmail.com.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
