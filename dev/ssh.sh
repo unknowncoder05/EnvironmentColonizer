9f071d039688fbf1acd55061f6e3bff6ad9c263c
unknowncoder05 <yersonlasso05@gmail.com>
2022-05-21 19:22:21 -0500

    kubectl

--- dev/ssh.sh
+++ dev/ssh.sh
@@ -1,4 +1,4 @@
-ssh-keygen -t ed25519 -C "yersonlasso05@gmail.com.com"
+ssh-keygen -t ed25519 -C "yersonlasso05@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 # https://docs.github.com/es/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
