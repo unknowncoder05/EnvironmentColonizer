679be2095a602ca3d825d5c37834db9e96b88885
Yerson <yersonlasso05@gmail.com>
2023-11-16 12:11:22 -0600

    Update homebrew.sh

--- dev/homebrew.sh
+++ dev/homebrew.sh
@@ -1,5 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/unknown-dev/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
 brew install gcc
